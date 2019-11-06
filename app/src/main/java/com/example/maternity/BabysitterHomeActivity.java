ced55a6a2eb8f03861dd8b135ccbfe811e6693d3
Rohan Verma <rsrohanverma@gmail.com>
2019-11-06 21:34:52 +0530

    All basic activities added.

---
+++ app/src/main/java/com/example/maternity/BabysitterHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.maternity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BabysitterHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_babysitter_home);
+    }
+}
---
+++ app/src/main/java/com/example/maternity/CreateDoctorAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.maternity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateDoctorAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_doctor_account);
+    }
+}
