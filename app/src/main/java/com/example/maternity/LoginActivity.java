c3e5fbae5ee1c024b0dd7d695085cf97cabfdf64
Rohan Verma <rsrohanverma@gmail.com>
2019-11-08 15:21:07 +0530

    Login Activity, Otp Verification Activity, Create Doctor Account.

--- app/src/main/java/com/example/maternity/LoginActivity.java
+++ app/src/main/java/com/example/maternity/LoginActivity.java
@@ -1,14 +1,56 @@
 package com.example.maternity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView createAccount;
 
+    AlertDialog.Builder alertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        createAccount = findViewById(R.id.createAccount);
+
+
+
+
+
+
+        alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Create Account as : ");
+        alertDialog.setCancelable(true);
+        alertDialog.setPositiveButton("Parent", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(getApplicationContext(), CreateParentAccountActivity.class));
+            }
+        });
+        alertDialog.setNegativeButton("Doctor", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(getApplicationContext(), CreateDoctorAccountActivity.class));
+            }
+        });
+        alertDialog.setNeutralButton("Babysitter", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(getApplicationContext(), CreateBabysitterAccountActivity.class));
+            }
+        });
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.show();
+            }
+        });
     }
 }
