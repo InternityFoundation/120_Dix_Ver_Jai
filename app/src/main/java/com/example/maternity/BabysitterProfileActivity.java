9343e423352a5329ad2127570d8c252809bde7a7
pratyu-j <pratyush021j@gmail.com>
2019-11-09 23:47:47 +0530

    all about parent side

--- app/src/main/java/com/example/maternity/BabysitterProfileActivity.java
+++ app/src/main/java/com/example/maternity/BabysitterProfileActivity.java
@@ -1,14 +1,95 @@
 package com.example.maternity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class BabysitterProfileActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference dbref;
+    String username, email, dob, aadhar, Address, rateHR, phone, aboutME, profilePic,availabity, gender, latitude, longitude;
+    private TextView USER, EMAIL, AADHAR, ADDRESS, RATE, PHONE, DOB, ABOUT_ME, GENDER, AVAILABILITY, LOCATION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_babysitter_profile);
+
+
+
+            USER = (TextView)findViewById(R.id.bbysittername);
+            EMAIL = (TextView)findViewById(R.id.bbysitteremail);
+            PHONE =  (TextView)findViewById(R.id.bbysitterphone);
+            AADHAR = (TextView)findViewById(R.id.bbysitteraadhar);
+            ADDRESS = (TextView)findViewById(R.id.bbysitteraddress);
+            DOB = (TextView)findViewById(R.id.bbysitterdob);
+            RATE = (TextView)findViewById(R.id.bbysitterrate);
+            ABOUT_ME = (TextView)findViewById(R.id.aboutme);
+            GENDER = (TextView)findViewById(R.id.bbysittegender);
+            AVAILABILITY = (TextView)findViewById(R.id.bbysitteravail);
+            LOCATION = (TextView)findViewById(R.id.bbysitterlocation);
+
+
+            firebaseDatabase = FirebaseDatabase.getInstance();
+            dbref = firebaseDatabase.getReference("USERS").child("NANNY").child("919521420343");
+            dbref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot)
+                    NannyDetails  nannyDetails = dataSnapshot.getValue(NannyDetails.class);
+                    username = nannyDetails.getUsername();
+                    email = nannyDetails.getEmail();
+                    phone = nannyDetails.getPhone();
+                    aadhar = nannyDetails.getAadharNo();
+                    dob = nannyDetails.getDOB();
+                    Address = nannyDetails.getPermanentAddress();
+                    rateHR = nannyDetails.getRateHr();
+                    aboutME = nannyDetails.getAboutMe();
+                    availabity = nannyDetails.getAvailablity();
+                    gender = nannyDetails.getGender();
+                    latitude = nannyDetails.getLatitude();
+                    longitude = nannyDetails.getLongitude();
+
+
+
+
+
+                    USER.setText("Username: " + username);
+                    EMAIL.setText("Email: " + email);
+                    PHONE.setText("Phone: " + phone);
+                    AVAILABILITY.setText("Availability: " + availabity);
+                    DOB.setText("Dob: " + dob);
+                    ADDRESS.setText("Address: " + Address);
+                    LOCATION.setText("Latitude, Longitude: " + latitude+ "," + longitude);
+                    RATE.setText("Rate/hr: " + rateHR);
+                    ABOUT_ME.setText("About me: " + aboutME);
+                    AADHAR.setText("Aadhar number: " + aadhar);
+                    GENDER.setText("gender: " + gender);
+
+
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(BabysitterProfileActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+
+
+
+        }
+
     }
-}
