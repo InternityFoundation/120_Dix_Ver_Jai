f7d5b635cced89fd3ed32cb5b67890a3f2a9b52c
Dprash <prashantdixit600@gmail.com>
2019-11-09 14:54:19 +0530

    Babysitter account layout added and implemented

---
+++ app/src/main/java/com/example/maternity/CreateBabySitterAccount2.java
@@ -0,0 +1,14 @@
+package com.example.maternity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateBabySitterAccount2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_baby_sitter_account2);
+    }
+}
--- app/src/main/java/com/example/maternity/CreateBabysitterAccountActivity.java
+++ app/src/main/java/com/example/maternity/CreateBabysitterAccountActivity.java
@@ -2,13 +2,29 @@ package com.example.maternity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateBabysitterAccountActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_babysitter_account);
+        Button btnNext = findViewById(R.id.button_babysitter_next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateBabysitterAccountActivity.this, CreateBabySitterAccount2.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
